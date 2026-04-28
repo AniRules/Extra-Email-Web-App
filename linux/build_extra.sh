bf93b46ccf5170cff2cd0f328899dbe22da99a96
Anirudh Poreddy <aporeddy043@rsdmo.org>
2026-04-27 19:47:46 -0500

    Update build_extra.sh for Linux AppImage only
    Removed Windows build process and added dependency installation.

--- linux/build_extra.sh
+++ linux/build_extra.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
+# Install dependencies
+sudo apt install -y libwebkit2gtk-4.1-dev libjavascriptcoregtk-4.1-dev libsoup-3.0-dev libssl-dev libgtk-3-dev libayatana-appindicator3-dev librsvg2-dev build-essential
 
-APP_NAME="ExtraEmail"
-TARGET_URL="https://extra.email"
+# Install pake
+sudo npm install -g pake-cli
 
-echo "--- Building Windows .exe ---"
-# Note: This creates a folder containing the .exe
-nativefier --name "$APP_NAME" \
-           --platform windows \
-           --arch x64 \
-           --single-instance \
-           "$TARGET_URL"
-
-echo "--- Building Linux AppImage ---"
-# This creates a single portable .AppImage file
-appnativefy --name "$APP_NAME" \
-            --url "$TARGET_URL"
-
-echo "--- Done! ---"
-echo "Windows folder is in: $(pwd)"
-echo "Linux AppImage is in: ~/appnativefy"
+# Build
+sudo pake https://extra.email --name Extra --inject inject-fix.js
